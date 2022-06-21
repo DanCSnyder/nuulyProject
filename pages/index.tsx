34786c868889ce6ec46a69477e134e77e305bef3
Dan Snyder <daniel.c.snyder@gmail.com>
2022-06-21 15:27:51 -0400

    [DS] Add initial index and api pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
 
-export default App;
+// export default App;
+
+const HomePage = () => {
+  return <div>Welcome to Next.js!!!!!!</div>;
+};
+
+export default HomePage;
