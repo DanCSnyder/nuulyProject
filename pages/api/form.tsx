54ff6eeb463cc0199e459b4ec2e98e8bf15347c4
Dan Snyder <daniel.c.snyder@gmail.com>
2022-06-24 13:08:42 -0400

    [DS] - Replace form call with testData

--- pages/api/form.tsx
+++ pages/api/form.tsx
@@ -1,19 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import testData from "testData";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body;
 
-  const url = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.MDB_API}&language=en-US&query=${body}&include_adult=false`;
+  // const url = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.MDB_API}&language=en-US&query=${body}&include_adult=false`;
 
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  
+  // const response = await fetch(url, {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
 
-  console.error("response?", await response.json());
+  // const results = await response.json();
 
-  res.status(200).json({ data: `sick` });
+  res.status(200).json({ data: testData });
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,39 @@
 import { AppProps } from "next/app";
+import test from "node:test";
 import { useState } from "react";
-
-const handleSubmit = async (event) => {
-  event.preventDefault();
-
-  const searchValue = event.target.search.value;
-
-  console.error({ searchValue });
-
-  const response = await fetch("/api/form", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(searchValue),
-  });
-
-  const result = await response.json();
-  console.error("result", result);
-};
+import { ActorType, MovieType } from "types";
 
 const HomePage = () => {
+  const [results, setResults] = useState<Array<ActorType> | null>(null);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const searchValue = event.target.search.value;
+
+    const response = await fetch("/api/form", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(searchValue),
+    });
+
+    const results = await response.json();
+    setResults(results.data.results.filter((result) => result.name));
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>SEARCH ME!</label>
-      <input type="text" id="search" name="search" required></input>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>SEARCH ME!</label>
+        <input type="text" id="search" name="search" required></input>
+      </form>
+      {results != null
+        ? results.map((result) => {
+            return <div>{result.name}</div>;
+          })
+        : null}
+    </>
   );
 };
 
