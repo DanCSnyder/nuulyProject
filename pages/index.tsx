52a865ad4a3da789d38662da9b94135963f5e2a4
Dan Snyder <daniel.c.snyder@gmail.com>
2022-06-25 12:06:37 -0400

    [DS] Add initial actor slug page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 import test from "node:test";
 import { useState } from "react";
 import { ActorType, MovieType } from "types";
+import Link from "next/link";
 
 const HomePage = () => {
   const [results, setResults] = useState<Array<ActorType> | null>(null);
@@ -19,7 +20,11 @@ const HomePage = () => {
     });
 
     const results = await response.json();
-    setResults(results.data.results.filter((result) => result.name));
+    setResults(
+      results.data.results.filter(
+        (result) => result.name && result.profile_path
+      )
+    );
   };
 
   return (
@@ -30,7 +35,16 @@ const HomePage = () => {
       </form>
       {results != null
         ? results.map((result) => {
-            return <div>{result.name}</div>;
+            return (
+              <div>
+                {result.name}
+                <Link href={`/actors/${result.id}`}>
+                  <img
+                    src={`http://image.tmdb.org/t/p/w300${result.profile_path}`}
+                  ></img>
+                </Link>
+              </div>
+            );
           })
         : null}
     </>
