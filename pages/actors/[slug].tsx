021996d1628bbcb56bb10512b8286b540388be44
Dan Snyder <daniel.c.snyder@gmail.com>
2022-06-27 11:01:59 -0400

    [DS] Move actors page to SSR

--- pages/actors/[slug].tsx
+++ pages/actors/[slug].tsx
@@ -1,10 +1,45 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import bradPittData from "bradPittData";
+import moreData from "moreData";
 
-const Post = () => {
-  const router = useRouter()
-  const { slug } = router.query
+const getCredits = (id: string) => {
+  const url = `https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=${process.env.MDB_API}&language=en-US`;
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
-  return <p>Post: {slug}</p>
+const getActorDetails = (id: string) => {
+  const url = `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.MDB_API}&language=en-US`;
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const Actors = ({ data }) => {
+  return <div>{data}</div>;
+};
+
+export async function getServerSideProps({ params: { slug } }) {
+  // const getData = await Promise.all([getActorDetails(slug), getCredits(slug)]);
+  // let results = await Promise.all(getData.map((r) => r.json()));
+  let results = [bradPittData, moreData];
+  results = results.reduce(
+    (accum, data) => ({
+      ...data,
+      ...accum,
+    }),
+    {}
+  );
+  return {
+    props: {
+      data: JSON.stringify(results),
+    },
+  };
 }
 
-export default Post
+export default Actors;
